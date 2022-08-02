71ee013b74f323dd2378baf86c34f99f8e327a6d
dimab <PShCDFhMEsS9mK3>
2022-08-02 22:13:49 +0700

    Добавьте файлы проекта.

---
+++ DomShori/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DomShori.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
